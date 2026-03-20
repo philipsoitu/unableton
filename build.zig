d732795493fd4e9e97debd96729f2a2532fb8a93
philipsoitu <philip.soitu@gmail.com>
2026-03-20 14:19:09 -0400

    unableton

--- build.zig
+++ build.zig
@@ -10,18 +10,18 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
-        .name = "ableton",
+        .name = "unableton",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
             .imports = &.{
-                // Here "ableton" is the name you will use in your source code to
-                // import this module (e.g. `@import("ableton")`). The name is
+                // Here "unableton" is the name you will use in your source code to
+                // import this module (e.g. `@import("unableton")`). The name is
                 // repeated because you are allowed to rename your imports, which
                 // can be extremely useful in case of collisions (which can happen
                 // importing modules from different packages).
-                // .{ .name = "ableton", .module = mod },
+                // .{ .name = "unableton", .module = mod },
             },
         }),
     });
